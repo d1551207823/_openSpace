bb9a2be71ecf7193609b15eedd333b7cc6333b7e
0xCode <1551207823@qq.com>
2025-02-11 16:24:22 +0800

    完成001

---
+++ Index_01.js
@@ -0,0 +1,71 @@
+// 实践 POW， 编写程序（编程语言不限）用自己的昵称 + nonce，不断修改nonce 进行 sha256 Hash 运算：
+// 直到满足 4 个 0 开头的哈希值，打印出花费的时间、Hash 的内容及Hash值。
+// 再次运算直到满足 5 个 0 开头的哈希值，打印出花费的时间、Hash 的内容及Hash值。
+
+// 题目#2
+// 实践非对称加密 RSA（编程语言不限）：使用crypto
+// 先生成一个公私钥对
+// 用私钥对符合 POW 4 个 0 开头的哈希值的 “昵称 + nonce” 进行私钥签名
+// 用公钥验证
+const crypto = require('crypto');
+const authors = '邓政坚'
+let nonce = 0;
+let start = new Date().getTime();
+let hash = '';
+let hashValue = '';
+let zero = '00000';
+let zero1 = '0000';
+let zeroResult = false
+let zeroResult1 = false
+const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
+    modulusLength: 1024,
+    publicKeyEncoding: {
+        type: 'spki',
+        format: 'pem'
+    },
+    privateKeyEncoding: {
+        type: 'pkcs8',
+        format: 'pem'
+    }
+})
+while (true) {
+    hash = authors + nonce;
+    hashValue = crypto.createHash
+    ('sha256').update(hash).digest('hex');
+    if (hashValue.startsWith(zero1) && !zeroResult) {
+        console.log('Zero:0000 => 花费时间：', (new Date().getTime() - start).toString() + 'ms');
+        console.log('Hash的内容：', hash);
+        console.log('Hash值：', hashValue);
+        zeroResult = true
+        // 签名
+        const sign = crypto.sign('sha256', Buffer.from(hash), {
+            key: privateKey,
+            padding: crypto.constants.RSA_PKCS1_PSS_PADDING
+        }).toString('base64')
+        console.log('签名：', sign);
+        // 验证签名
+        const verify = crypto.verify('sha256', Buffer.from(hash), {
+            key: publicKey,
+            padding: crypto.constants.RSA_PKCS1_PSS_PADDING
+        }, Buffer.from(sign, 'base64'))
+        console.log('验证签名：', verify,'\n');
+
+
+
+
+    }
+    if (hashValue.startsWith(zero)) {
+        console.log('Zero:00000 =>花费时间：', (new Date().getTime() - start).toString() + 'ms');
+        console.log('Hash的内容：', hash);
+        console.log('Hash值：', hashValue);
+        zeroResult1 = true
+    }
+    nonce++;
+    if (zeroResult && zeroResult1) {
+        break;
+    }
+}
+
+
+
+
